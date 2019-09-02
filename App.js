ee4f7917646db3564ba2d522d4aed949cf7f8550
LEE INHAK <inhak9@naver.com>
2019-09-02 13:38:56 +0900

    login screen.js

---
+++ App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import LoginScreen from './screens/LoginScreen'
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text> CAR ACTION</Text>
+      <IconTextInput>
+
+      </IconTextInput>
+      <IconTextInput>
+        
+      </IconTextInput>
+      <RoundButton />
+  
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
